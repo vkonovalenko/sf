c1fc14b9807edb8b50af5eb783611c1e9bfff4c6
Vyacheslav Konovalenko <vyacheslav.konovalenko@ideus.biz>
2017-04-03 19:34:42 +0300

    ilocator changes

--- appname/modules/abstracts/locators/ILocator.py
+++ appname/modules/abstracts/locators/ILocator.py
@@ -5,9 +5,9 @@ class ILocator(ABC):
 
     _classes = {}
 
-    def get_class(self, alias):
-        result = self._classes.get(alias)
-        if result is not None:
-            return result
-        else:
-            raise Exception(alias + ' not found in locator.')
+    @abstractmethod
+    def __init(self):
+        pass
+
+    def get_classes(self):
+        return self._classes
