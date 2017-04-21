b2ec7372ed670b51cd5f407dfe68b036bf0af0a3
Vyacheslav Konovalenko <vyacheslav.konovalenko@ideus.biz>
2017-04-21 12:50:25 +0300

    exception message fix for not found controller's action

--- appname/modules/controller/Controller.py
+++ appname/modules/controller/Controller.py
@@ -1,4 +1,5 @@
 from abc import ABC
+import os, sys
 
 
 class Controller(ABC):
@@ -13,4 +14,6 @@ class Controller(ABC):
         if hasattr(self, command):
             getattr(self, command)()
         else:
-            raise Exception('command ' + command + ' not found in controller.')
+            controller_name = self.__class__.__name__;
+            child_path = sys.modules[self.__class__.__module__].__file__
+            raise Exception('command ' + command + ' not found in controller ' + controller_name + ': ' + child_path)
