060f79be8348380c0a786ab7595247c45c17d9d0
Vyacheslav Konovalenko <vyacheslav.konovalenko@ideus.biz>
2017-04-21 19:55:40 +0300

    refactoring

--- appname/modules/request/classes/Websocket.py
+++ appname/modules/request/classes/Websocket.py
@@ -1,24 +1,23 @@
 from appname.modules.request.IRequest import IRequest
 import json
 
+# @TODO: delete dependency from concrete Json format
 
 class Websocket(IRequest):
-
-    _json_data = {}
 
     def init(self):
         pass
 
     def handle_params(self, params=None):
         try:
-            self._json_data = json.loads(params)
+            self._data = json.loads(params)
         except:
             pass
 
-        data = self._json_data.get('data')
+        data = self._data.get('data')
         if data is None or type(data) is not dict:
             data = {}
         self._data = data
 
     def command(self):
-        return self._json_data.get('command')
+        return self._data.get('command')
